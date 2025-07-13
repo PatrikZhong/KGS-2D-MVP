d50cea66303760e142ae631b5c2d3dfd5613d4b6
If I were a green onion <62708684+Galimatiash@users.noreply.github.com>
2025-07-13 12:49:16 +0200

    playablecharacters

--- scripts/playable_character_1.gd
+++ scripts/playable_character_1.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @onready var attack_range = get_node("attack_range")
 @onready var attack_timer = get_node("Timer")
 @onready var sprite = $unit_sprite
+@onready var hp_bar = $Label
 
 var speed = 500
 var click_position = Vector2()
@@ -16,11 +17,18 @@ var can_attack = true
 enum state {
 	idle,
 	walk,
-	attack
+	attack,
+	death
 }
 
+
 func take_damage(damage: int):
-	print("YOU JUST TOOK ", damage)
+	health = health - damage
+	hp_bar.text = str(health)
+
+	if health <= 0:
+		updateAnimation(state.death)
+		queue_free()
 
 func _ready():
 	updateAnimation(current_state)
@@ -34,13 +42,16 @@ func updateAnimation(input: state):
 			sprite.play("attack")
 		state.walk:
 			sprite.play("walk")
+		state.death:
+			sprite.play("death")
 	
 func _physics_process(delta):
+	
 	updateAnimation(current_state)
 	
 	if Input.is_action_just_pressed("left_mouse"):
 		if character_selector.active == 1:
-			click_position = get_global_mouse_position()
+			click_position = get_parent().to_local(get_global_mouse_position())
 		
 	if position.distance_to(click_position) > 5:
 		target_position = (click_position - position).normalized()
@@ -55,14 +66,12 @@ func _physics_process(delta):
 		updateAnimation(state.idle)
 		
 	if attack_range.has_overlapping_bodies():
-		
 		updateAnimation(state.attack)
 		if can_attack:
 			can_attack = false
 			attack_timer.start()
 			for i in attack_range.get_overlapping_bodies():
-				i.take_damage(5)
-		
+				i.take_damage(10)
 	elif current_state != state.walk:
 		updateAnimation(state.idle)
 
@@ -70,7 +79,5 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 	if Input.is_action_just_pressed("left_mouse"):
 		print("selected this unit lol")
 
-
 func _on_timer_timeout() -> void:
-	print("timer timed out")
 	can_attack = true
--- scripts/playable_character_2.gd
+++ scripts/playable_character_2.gd
@@ -1,53 +1,56 @@
 extends CharacterBody2D
 
 @onready var character_selector = get_parent()
-@onready var sprite = $unit_sprite
 @onready var attack_range = get_node("attack_range")
-@onready var cooldown = $CooldownTimer
+@onready var attack_timer = get_node("Timer")
+@onready var sprite = $unit_sprite
 @onready var hp_bar = $Label
 
 var speed = 500
 var click_position = Vector2()
 var target_position = Vector2()
-var currentState = state.idle
-var isRunning = false
+var current_state = state.idle
 
 var health = 100
+var can_attack = true
 
 enum state {
 	idle,
 	walk,
-	attack
+	attack,
+	death
 }
 
 func take_damage(damage: int):
 	health = health - damage
 	hp_bar.text = str(health)
-	
-	print("YOU JUST TOOK ", damage, " DAMAGE! Remaining: ", health)
+
 	if health < 0:
+		updateAnimation(state.death)
 		queue_free()
 	
 
 func _ready():
-	updateAnimation(currentState)
+	updateAnimation(current_state)
 
 func updateAnimation(input: state):
-	currentState = input
-	match currentState:
+	current_state = input
+	match current_state:
 		state.idle:
 			sprite.play("idle")
 		state.attack:
 			sprite.play("attack")
 		state.walk:
 			sprite.play("walk")
+		state.death:
+			sprite.play("death")
 	
 func _physics_process(delta):
-	updateAnimation(currentState)
+	updateAnimation(current_state)
 	
 	if Input.is_action_just_pressed("left_mouse"):
 		if character_selector.active == 2:
-			click_position = get_global_mouse_position()
+			click_position = get_parent().to_local(get_global_mouse_position())
 		
 	if position.distance_to(click_position) > 5:
 		target_position = (click_position - position).normalized()
@@ -60,12 +63,12 @@ func _physics_process(delta):
 		elif velocity.x > 0:
 			sprite.flip_h = false
 			
-	elif currentState != state.attack:
+	elif current_state != state.attack:
 		updateAnimation(state.idle)
 		
 	if attack_range.has_overlapping_bodies():
 		updateAnimation(state.attack)
-	elif currentState != state.walk:
+	elif current_state != state.walk:
 		updateAnimation(state.idle)
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
